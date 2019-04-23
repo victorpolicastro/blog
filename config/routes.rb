b37fb0277e392f0771452848b689675f1c3d14b8
victorpolicastro <policastro.victor@gmail.com>
2019-04-23 14:35:43 -0300

    User's historic

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :categories
-
+  resources :historic
+  
   root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
